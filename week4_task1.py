33aef2de3bf020de7571db1e52b58072f3a0d17a
qpoe <95753649+qpoe@users.noreply.github.com>
2024-12-08 20:40:46 +0500

    Update week4_task1.py

--- week4_task1.py
+++ week4_task1.py
@@ -4,13 +4,17 @@ size = width, height = 500, 500
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 running = True
+pos = []
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    x, y = pygame.mouse.get_pos()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            pos.append((mouse_x, mouse_y))
     screen.fill("white")
-    pygame.draw.circle(screen, "red", (x, y), 30)
+    for i in pos:
+        pygame.draw.circle(screen, "red", i, 10)
     pygame.display.update()
     clock.tick(60)
 pygame.quit()
